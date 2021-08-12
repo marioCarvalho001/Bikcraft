a18600e4aacb16ed6154d973437e5e88e8258aa8
marioCarvalho001 <84389288+marioCarvalho001@users.noreply.github.com>
2021-08-12 07:07:18 -0300

    Um pouco de Php.

--- JS/script.js
+++ JS/script.js
@@ -13,3 +13,12 @@ new SimpleSlide({
 if(window.SimpleAnime) {
 new SimpleAnime();
 }
+
+if(window.SimpleForm){
+new SimpleForm({
+  form:"formphp", //seletor de formulário
+  button:"#enviar", //seletor do botão
+  erro: "<div id='form-erro'><h2> Erro no envio!</h2><p>Um erro ocorreu, tente para o email mariocarvalho755@gmail.com</p></div>",
+  sucesso:"<div id='form-sucesso><h2>Formulario enviado com sucesso</h2><p> em breve eu entro em contato</p></div>",
+});
+}
